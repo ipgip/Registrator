d55498bcfc4c7c6d5a8b380bfe90f1404749216d
Игорь <ipigp@yandex.ru>
2018-11-09 18:53:49 +0300

    keyboard

---
+++ Registrator/Keyboard.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registrator
+{
+    public partial class Keyboard : UserControl
+    {
+        private event EventHandler<KeyArgs> _Click;
+        private EventHandler<KeyArgs> _BSP;
+        private EventHandler<KeyArgs> _Ent;
+
+        char[][] kbd = new char[][]{ new char[] { '1','2','3','4','5','6','7','8','9','0','Ё','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Ь','Б','Ю','.','-' },
+                                     new char[] { '1','2','3','4','5','6','7','8','9','0','_','Q','W','E','R','T','Y','U','I','O','P','*','A','S','D','F','G','H','J','K','L','#','%','Z','X','C','V','B','N','M','[',']','.','@' } };
+        bool LanguageSelector = false; // Латинский
+
+        #region События
+        [Category("IGP"), Description("Нажата клавиша")]
+        public event EventHandler<KeyArgs> ClickChar
+        {
+            add { _Click += value; }
+            remove { _Click -= value; }
+        }
+
+        [Category("IGP"), Description("Нажат Backspece")]
+        public event EventHandler<KeyArgs> BSPChar
+        {
+            add { _BSP += value; }
+            remove { _BSP -= value; }
+        }
+        [Category("IGP"), Description("Нажат ВВОД")]
+        public event EventHandler<KeyArgs> ENTChar
+        {
+            add { _Ent += value; }
+            remove { _Ent -= value; }
+        }
+        #endregion
+
+        public Keyboard()
+        {
+            InitializeComponent();
+            //button36.Text = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            _Click?.Invoke(this, new KeyArgs((sender as Button).Text[0]));
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            _BSP?.Invoke(this, new KeyArgs(' '));
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            _Ent?.Invoke(this, new KeyArgs(' '));
+        }
+
+        // переключение языков клавиатуры
+        private void LanguageSelector_Click(object sender, EventArgs e)
+        {
+            LanguageSelector = !LanguageSelector;
+            LoadKeys();
+        }
+
+        private void Keyboard_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < kbd[LanguageSelector ? 0 : 1].Length; i++)
+            {
+                Button b = new Button
+                {
+                    Font=new Font(DefaultFont.FontFamily, 8, FontStyle.Bold),
+                    Text = kbd[LanguageSelector ? 0 : 1][i].ToString(),
+                    Dock = DockStyle.Fill
+                };
+                b.Click += button_Click;
+                tableLayoutPanel1.Controls.Add(b, i % 11, i / 11);
+            }
+            Lang.Text = LanguageSelector ? "RUS" : "LAT";
+        }
+
+        // загрузка клавиш
+        private void LoadKeys()
+        {
+            for (int i = 0; i < kbd[LanguageSelector ? 0 : 1].Length; i++)
+            {
+                Button b = tableLayoutPanel1.GetControlFromPosition(i % 11, i / 11) as Button;
+
+                b.Text = kbd[LanguageSelector ? 0 : 1][i].ToString();
+            }
+            Lang.Text = LanguageSelector ? "RUS" : "LAT";
+        }
+    }
+
+    public class KeyArgs : EventArgs
+    {
+        public char Key;
+
+        public KeyArgs(char p)
+        {
+            Key = p;
+        }
+    }
+
+}
---
+++ Registrator/Keyboard.designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Registrator
+{
+    partial class Keyboard
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button51 = new System.Windows.Forms.Button();
+            this.BSP = new System.Windows.Forms.Button();
+            this.ENT = new System.Windows.Forms.Button();
+            this.Lang = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 12;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayoutPanel1.Controls.Add(this.button51, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.BSP, 11, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ENT, 11, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Lang, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(445, 149);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button51
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button51, 7);
+            this.button51.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button51.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button51.Location = new System.Drawing.Point(77, 119);
+            this.button51.Name = "button51";
+            this.button51.Size = new System.Drawing.Size(253, 27);
+            this.button51.TabIndex = 50;
+            this.button51.Text = " ";
+            this.button51.UseVisualStyleBackColor = true;
+            this.button51.Click += new System.EventHandler(this.button_Click);
+            // 
+            // BSP
+            // 
+            this.BSP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BSP.Location = new System.Drawing.Point(410, 3);
+            this.BSP.Name = "BSP";
+            this.BSP.Size = new System.Drawing.Size(32, 23);
+            this.BSP.TabIndex = 59;
+            this.BSP.Text = "˂";
+            this.BSP.UseVisualStyleBackColor = true;
+            this.BSP.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // ENT
+            // 
+            this.ENT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ENT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ENT.Location = new System.Drawing.Point(410, 32);
+            this.ENT.Name = "ENT";
+            this.tableLayoutPanel1.SetRowSpan(this.ENT, 4);
+            this.ENT.Size = new System.Drawing.Size(32, 114);
+            this.ENT.TabIndex = 60;
+            this.ENT.Text = "ВВОД";
+            this.ENT.UseVisualStyleBackColor = true;
+            this.ENT.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // Lang
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.Lang, 2);
+            this.Lang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Lang.Location = new System.Drawing.Point(3, 119);
+            this.Lang.Name = "Lang";
+            this.Lang.Size = new System.Drawing.Size(68, 27);
+            this.Lang.TabIndex = 63;
+            this.Lang.Text = "Rus/Lat";
+            this.Lang.UseVisualStyleBackColor = true;
+            this.Lang.Click += new System.EventHandler(this.LanguageSelector_Click);
+            // 
+            // Keyboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Keyboard";
+            this.Size = new System.Drawing.Size(445, 149);
+            this.Load += new System.EventHandler(this.Keyboard_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button51;
+        private System.Windows.Forms.Button BSP;
+        private System.Windows.Forms.Button ENT;
+        private System.Windows.Forms.Button Lang;
+    }
+}
